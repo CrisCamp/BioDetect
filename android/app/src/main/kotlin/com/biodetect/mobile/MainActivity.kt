e103ebdb5fac5b740d6f358de857e3e7a104511b
Miguel Ventura <miguelforce.1218@gmail.com>
2025-10-05 19:19:02 -0600

    Se hicieron pequeñas correcciones para el correcto funcionamiento con el cambio de nombre del paquete y de nombre de la app

---
+++ android/app/src/main/kotlin/com/biodetect/mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.biodetect.mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/example/biodetect/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.biodetect
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
